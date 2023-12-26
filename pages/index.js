0c4ff46de665f8e476542045038b2c84dac11eb4
JP-sDEV <jon.pablo80@gmail.com>
2023-12-26 13:23:22 -0500

    feat: added 'experience' section

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Contact from '@/components/Contact';
-import Head from 'next/head'
+import Head from 'next/head';
 import Hero from '@/components/Hero';
 import Projects from '@/components/Projects';
+import Experience from '@/components/Experience';
 import Stack from '@/components/Stack';
 import Education from '@/components/Education';
 import { meta } from '@/data/config';
@@ -15,6 +16,7 @@ export default function Index() {
       <div className="p-6 lg:p-20 max-w-4xl m-auto w-fit">
         <Hero />
         <div className="p-0 space-y-10">
+          <Experience />
           <Projects />
           <Stack />
           <Education />
