fb53d3ce5634c2b59eae8f61ca12612d88ddc965
JP-sDEV <jon.pablo80@gmail.com>
2023-12-26 13:21:55 -0500

    refactor: format update

--- components/Education.js
+++ components/Education.js
@@ -12,25 +12,26 @@ export default function Education() {
       <div className="mt-0">
         {education.education.map((item, index) => {
           return (
-            <div className="flex flex-col p-6 border border-lightText rounded-xl mb-4 dark:hover-border-orange-100" key={index}>
-
-            <div className="flex flex-row justify-between">
-            
-              <h3 className=''>{item.school}</h3>
-              <p className=''>{item.start_year} - {item.graduation_year}</p>
+            <div
+              className="flex flex-col p-6 border border-lightText rounded-xl mb-4 dark:hover-border-orange-100"
+              key={index}
+            >
+              <div className="flex flex-row justify-between">
+                <h3 className="">{item.school}</h3>
+                <p className="">
+                  {item.start_year} - {item.graduation_year}
+                </p>
               </div>
 
               <div className=" -m-3 pt-5">
                 {item.majors && (
-                <div className="flex flex-row   py-1 px-3">
+                  <div className="flex flex-row py-1 px-3">
                     <span className=" text-lightText  transition-colors duration-500 ellipsis">
-                        {item.majors}
+                      {item.majors}
                     </span>
-                </div>
-
+                  </div>
                 )}
               </div>
-            
             </div>
           );
         })}
